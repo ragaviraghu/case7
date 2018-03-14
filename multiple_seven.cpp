c35c9aea58e7fd238410084f5ff02ad9d6309426
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-14 09:56:13 -0700

    Create multiple_seven.cpp

---
+++ multiple_seven.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{int N;
+cout<<"Enter the number : "<<endl;
+cin>>N;
+if(N%7==0)
+cout<<"The given number is a multiple of 7 "<<endl;
+else
+cout<<"The given number is not a multiple of 7 "<<endl;
+return 0;
+}
