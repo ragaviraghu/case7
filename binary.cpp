2f72ee2e6646c48430993b42490b8417063c555f
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-15 09:48:37 -0700

    Create binary.cpp

---
+++ binary.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+string s;
+cout<<"Enter the string : "<<endl;
+cin>>s;
+int i,flag=0,len;
+len=s.size();
+for(i=0;i<len;i++)
+{
+if(s[i]=='1'||s[i]=='0')
+{
+flag=1;
+}
+else
+{
+flag=0;
+break;
+}
+}
+if(flag==1)
+cout<<"Yes(given string is in binary format)"<<endl;
+else
+cout<<"No";
+}
